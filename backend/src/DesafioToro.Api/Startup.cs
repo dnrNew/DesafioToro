0ebbb00035557eed4c6dc38c0da2331315d484d3
Diego Nogueira <dnr.new@gmail.com>
2022-09-20 23:48:39 -0300

    Adiciona configurações do Docker para o back end
    Adiciona arquivos e configurações do Docker para o projeto de back end,
    ajusta portas e url.
    
    Ajusta nome da projeto de API na solution para Api minúsculo.
    
    Ajusta configurações da conexão de banco de dados para integrar com
    o Docker.

--- backend/src/DesafioToro.Api/Startup.cs
+++ backend/src/DesafioToro.Api/Startup.cs
@@ -34,12 +34,8 @@ builder.Services.AddCors(options =>
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
+app.UseSwagger();
+app.UseSwaggerUI();
 app.UseCors(desafioToroOrigins);
 app.UseHttpsRedirection();
 app.UseAuthorization();
